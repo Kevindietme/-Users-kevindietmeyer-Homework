0066b7e0305d4a3bcb6160bd828f8e48b01aa104
Kevin Dietmeyer <kevindietmeyer@Kevins-MacBook-Air-2.local>
2023-06-11 21:39:07 -0400

    These are two basic examples of loops, a for loop with an if statement and loading with a constraint.

---
+++ loops.js
@@ -0,0 +1,18 @@
+// const names = ['Ed','John','Maria','Eliza','Burrito','Harry','Potter'];
+
+// for (name of names){
+//    console.log(`hello there ${name}`);
+//}
+
+//for (name of names){
+//    if(name === 'Maria'){
+//        console.log("Maria is in my list!");
+//        break;
+//    }
+//}
+
+let loading = 0;
+while (loading < 100){
+    console.log('Website is still loading');
+    loading++;
+}
